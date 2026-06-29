44d778c71fdf6792d88301b938440c7f7c5b4d59
Tal Kohavy <talkohavy@gmail.com>
2026-06-29 16:19:27 +0300

    ran the reset project script

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,14 +1,5 @@
-import { useColorScheme } from 'react-native';
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { Stack } from "expo-router";
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
-  );
+export default function RootLayout() {
+  return <Stack />;
 }
