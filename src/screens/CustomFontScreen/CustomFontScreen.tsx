de9b81a18e161fd8740c011dc548ca4b1b1a63c6
Tal Kohavy <talkohavy@gmail.com>
2026-07-13 11:49:53 +0300

    About screen converted to CustomFontScreen

--- src/screens/CustomFontScreen/CustomFontScreen.tsx
+++ src/screens/CustomFontScreen/CustomFontScreen.tsx
@@ -1,10 +1,12 @@
 import { Text, View, StyleSheet } from 'react-native';
 import { Theme } from '../../common/constants';
 
-export default function AboutScreen() {
+export default function CustomFontScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>About Expo.</Text>
+      <Text style={styles.text}>Lucky Love</Text>
+      <Text style={styles.text}>Custom Font</Text>
+      <Text style={styles.text}>Master</Text>
     </View>
   );
 }
@@ -12,13 +14,16 @@ export default function AboutScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingVertical: 40,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     backgroundColor: Theme.light.colors.base_0,
   },
   text: {
+    letterSpacing: 1,
     color: Theme.light.colors.base_100,
     fontSize: Theme.fontSizes.xl_8,
     fontWeight: 'semibold',
+    fontFamily: 'Matemasie_400Regular',
   },
 });
